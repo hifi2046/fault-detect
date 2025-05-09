0e89ee9b76ce6d94e4e7558c9c567db1871d0330
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-09 16:33:01 +0800

    mapper

--- src/main/java/com/hifiax/fault_detect/FaultDetectApplication.java
+++ src/main/java/com/hifiax/fault_detect/FaultDetectApplication.java
@@ -2,7 +2,9 @@ package com.hifiax.fault_detect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.hifiax.fault_detect.dao")
 @SpringBootApplication
 public class FaultDetectApplication {
 
---
+++ src/main/java/com/hifiax/fault_detect/dao/TradeMapper.java
@@ -0,0 +1,7 @@
+package com.hifiax.fault_detect.dao;
+
+import com.hifiax.fault_detect.data.Trade;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface TradeMapper extends BaseMapper<Trade> {
+}
