79f7e684076a5ad7120f3f6ffd4e4a6728a9499b
huangyinfei <hifi00@sina.com>
2025-05-09 21:24:42 +0800

    assert

--- src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
+++ src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
@@ -5,6 +5,7 @@ import com.hifiax.fault_detect.data.Trade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FaultDetectApplicationTests {
@@ -13,8 +14,14 @@ class FaultDetectApplicationTests {
     private TradeMapper tradeMapper;
     @Test
     void contextLoads() {
+    }
+    @Test
+    void tc001_db_trade_check() {
         Trade trade=tradeMapper.selectById(1);
         System.out.println(trade);
+        assertThat(trade.getAccount()).isEqualTo("A123456789");
+        assertThat(trade.getAvgAmount()).isEqualTo("100.00");
+        assertThat(trade.getLastGpsX()).isEqualTo(1020.0f);
     }
 
 }
