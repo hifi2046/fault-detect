73947d721a7743713b832772bf9decaf2d847e85
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-09 17:22:16 +0800

    test

--- src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
+++ src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
@@ -1,13 +1,19 @@
 package com.hifiax.fault_detect;
 
+import com.hifiax.fault_detect.dao.TradeMapper;
+import com.hifiax.fault_detect.data.Trade;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FaultDetectApplicationTests {
 
+    @Autowired
+    private TradeMapper tradeMapper;
     @Test
     void contextLoads() {
+        Trade trade=tradeMapper.selectById(1);
+        System.out.println(trade);
     }
 
 }
