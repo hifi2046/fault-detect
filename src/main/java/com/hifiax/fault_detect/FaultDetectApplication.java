c345252802b67510399674c61592149099a07c8d
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-09 10:01:30 +0800

    init

---
+++ src/main/java/com/hifiax/fault_detect/FaultDetectApplication.java
@@ -0,0 +1,13 @@
+package com.hifiax.fault_detect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultDetectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaultDetectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hifiax.fault_detect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultDetectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
