5249d61c1522b02c95311537cbb8b15781fe9864
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-10 22:19:41 +0800

    schedule

--- src/main/java/com/hifiax/fault_detect/FaultDetectApplication.java
+++ src/main/java/com/hifiax/fault_detect/FaultDetectApplication.java
@@ -3,6 +3,7 @@ package com.hifiax.fault_detect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.hifiax.fault_detect.dao")
 @SpringBootApplication
