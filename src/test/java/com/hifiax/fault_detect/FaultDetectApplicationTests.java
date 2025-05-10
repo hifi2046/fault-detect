dde188b38c30ab30cb0d17e78e9b39a52093ab5a
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-10 19:37:54 +0800

    rule str

--- src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
+++ src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
@@ -45,28 +45,28 @@ class FaultDetectApplicationTests {
     void tc004_check_amount_normal() {
         String msg="2|A123456789|W|150.00|100.00|40.00|1000.0|1000.0|1020.0|1020.0|2025-05-09 12:20|2025-05-09 12:10";
         Trade trade=new Trade(msg);
-        assertThat(RuleJudge.checkAmount(trade)).isEqualTo(false);
+        assertThat(RuleJudge.checkAmount(trade)).isEqualTo("");
     }
 
     @Test
     void tc005_check_amount_break() {
         String msg="3|A123456789|W|1000.00|100.00|40.00|1000.0|1000.0|1020.0|1020.0|2025-05-09 12:30|2025-05-09 12:20";
         Trade trade=new Trade(msg);
-        assertThat(RuleJudge.checkAmount(trade)).isEqualTo(true);
+        assertThat(RuleJudge.checkAmount(trade)).isNotEqualTo("");
     }
 
     @Test
     void tc006_check_location_normal() {
         String msg="2|A123456789|W|150.00|100.00|40.00|1000.0|1000.0|1020.0|1020.0|2025-05-09 12:20|2025-05-09 12:10";
         Trade trade=new Trade(msg);
-        assertThat(RuleJudge.checkGps(trade)).isEqualTo(false);
+        assertThat(RuleJudge.checkGps(trade)).isEqualTo("");
     }
 
     @Test
     void tc007_check_location_break() {
         String msg="4|A123456789|W|100.00|100.00|40.00|2000.0|2000.0|1020.0|1020.0|2025-05-09 12:40|2025-05-09 12:30";
         Trade trade=new Trade(msg);
-        assertThat(RuleJudge.checkGps(trade)).isEqualTo(true);
+        assertThat(RuleJudge.checkGps(trade)).isNotEqualTo("");
     }
 
 }
