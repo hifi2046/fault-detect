93f38af38fff964998f81d09ab53c111499d80e4
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-10 14:20:58 +0800

    tc002

--- src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
+++ src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
@@ -2,6 +2,7 @@ package com.hifiax.fault_detect;
 
 import com.hifiax.fault_detect.dao.TradeMapper;
 import com.hifiax.fault_detect.data.Trade;
+import com.hifiax.fault_detect.mq.Receiver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,9 +13,11 @@ class FaultDetectApplicationTests {
 
     @Autowired
     private TradeMapper tradeMapper;
+    private Receiver receiver;
     @Test
     void contextLoads() {
     }
+
     @Test
     void tc001_db_trade_check() {
         Trade trade=tradeMapper.selectById(1);
@@ -22,6 +25,11 @@ class FaultDetectApplicationTests {
         assertThat(trade.getAccount()).isEqualTo("A123456789");
         assertThat(trade.getAvgAmount()).isEqualTo("100.00");
         assertThat(trade.getLastGpsX()).isEqualTo(1020.0f);
+    }
+
+    @Test
+    void tc002_mq_receive_check() {
+        receiver.receive();
     }
 
 }
