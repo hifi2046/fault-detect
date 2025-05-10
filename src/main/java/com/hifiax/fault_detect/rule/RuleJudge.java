bbb1f4119fbe447223fa3de37c9b3cca857d60a9
huangyinfei <hifi00@sina.com>
2025-05-10 19:31:21 +0800

    tc004~007

--- src/main/java/com/hifiax/fault_detect/rule/RuleJudge.java
+++ src/main/java/com/hifiax/fault_detect/rule/RuleJudge.java
@@ -4,11 +4,21 @@ import com.hifiax.fault_detect.data.Trade;
 
 public class RuleJudge {
 
-    public void checkAmount(Trade trade) {
-
+    public static boolean checkAmount(Trade trade) {
+        if( trade.getAmount().doubleValue() > trade.getAvgAmount().doubleValue()+trade.getStd().doubleValue()*3) {
+            System.out.println("Alert: amount rule broken!");
+            return true;
+        }
+        return false;
     }
 
-    public void checkGps(Trade trade) {
-
+    public static boolean checkGps(Trade trade) {
+        Float deltaX = trade.getGpsX() - trade.getLastGpsX();
+        Float deltaY = trade.getGpsY() - trade.getLastGpsY();
+        if( deltaX * deltaX + deltaY * deltaY > 1000000.0 ) {
+            System.out.println("Alert: location rule broken!");
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
+++ src/test/java/com/hifiax/fault_detect/FaultDetectApplicationTests.java
@@ -3,6 +3,7 @@ package com.hifiax.fault_detect;
 import com.hifiax.fault_detect.dao.TradeMapper;
 import com.hifiax.fault_detect.data.Trade;
 import com.hifiax.fault_detect.mq.Receiver;
+import com.hifiax.fault_detect.rule.RuleJudge;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,6 +39,34 @@ class FaultDetectApplicationTests {
         String msg="2|A123456789|W|150.00|100.00|40.00|1000.0|1000.0|1020.0|1020.0|2025-05-09 12:20|2025-05-09 12:10";
         Trade trade=new Trade(msg);
         System.out.println(trade);
+    }
+
+    @Test
+    void tc004_check_amount_normal() {
+        String msg="2|A123456789|W|150.00|100.00|40.00|1000.0|1000.0|1020.0|1020.0|2025-05-09 12:20|2025-05-09 12:10";
+        Trade trade=new Trade(msg);
+        assertThat(RuleJudge.checkAmount(trade)).isEqualTo(false);
+    }
+
+    @Test
+    void tc005_check_amount_break() {
+        String msg="3|A123456789|W|1000.00|100.00|40.00|1000.0|1000.0|1020.0|1020.0|2025-05-09 12:30|2025-05-09 12:20";
+        Trade trade=new Trade(msg);
+        assertThat(RuleJudge.checkAmount(trade)).isEqualTo(true);
+    }
+
+    @Test
+    void tc006_check_location_normal() {
+        String msg="2|A123456789|W|150.00|100.00|40.00|1000.0|1000.0|1020.0|1020.0|2025-05-09 12:20|2025-05-09 12:10";
+        Trade trade=new Trade(msg);
+        assertThat(RuleJudge.checkGps(trade)).isEqualTo(false);
+    }
+
+    @Test
+    void tc007_check_location_break() {
+        String msg="4|A123456789|W|100.00|100.00|40.00|2000.0|2000.0|1020.0|1020.0|2025-05-09 12:40|2025-05-09 12:30";
+        Trade trade=new Trade(msg);
+        assertThat(RuleJudge.checkGps(trade)).isEqualTo(true);
     }
 
 }
