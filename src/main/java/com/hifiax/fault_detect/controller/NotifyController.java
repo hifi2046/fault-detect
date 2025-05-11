2972e370865522d694b7d49810c834cd24d99ec3
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-11 08:20:26 +0800

    notify

---
+++ src/main/java/com/hifiax/fault_detect/controller/NotifyController.java
@@ -0,0 +1,31 @@
+package com.hifiax.fault_detect.controller;
+
+import com.hifiax.fault_detect.data.Trade;
+import com.hifiax.fault_detect.mq.Receiver;
+import com.hifiax.fault_detect.rule.RuleJudge;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+public class NotifyController {
+
+    @Autowired
+    Receiver receiver;
+
+    @Autowired
+    RuleJudge judge;
+
+    @PutMapping("/notify")
+    public String notify() {
+        String msg=receiver.receive();
+        if(msg != "") {
+            Trade trade=new Trade(msg);
+            judge.checkAllRules(trade);
+        }
+        return "ok";
+    }
+}
