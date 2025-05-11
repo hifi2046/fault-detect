1b926b5ccb68a2b64e1cba7646847bd69ae12389
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-11 08:59:28 +0800

    alert controller

--- src/main/java/com/hifiax/fault_detect/controller/AlertController.java
+++ src/main/java/com/hifiax/fault_detect/controller/AlertController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
